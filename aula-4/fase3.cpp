e342cd6ff4ed398caffb9929d07ec29e054d92de
gabrielbonassi-controlid <gabriel.bonassi@controlid.com.br>
2024-01-23 18:34:49 -0300

    Aula 4

--- aula-4/fase3.cpp
+++ aula-4/fase3.cpp
@@ -37,7 +37,7 @@ int best_template_size(int last_size, Mat_<FLT> img_ref, Mat_<FLT> next_frame_fl
     }
 
     for (int i = min; i < max; i = i + 5) { // pula de 5 em 5 pxl pq ngm merece
-        resize(img_ref, img_ref_temp, Size(i, i), 0, 0, INTER_AREA);
+        resize(img_ref, img_ref_temp, Size(i, i), 0, 0, INTER_NEAREST);
         img_ref_temp = somaAbsDois(dcReject(img_ref_temp, 1.0));
         matchTemplate(next_frame_flt, img_ref_temp, result, CV_TM_CCOEFF_NORMED);
         minMaxLoc(result, &min_max.min_val, &min_max.max_val, &min_max.min_loc, &min_max.max_loc);
@@ -50,9 +50,9 @@ int best_template_size(int last_size, Mat_<FLT> img_ref, Mat_<FLT> next_frame_fl
     return size;
 }
 
-void draw_box(int size, Mat_<FLT>& quadrado, Mat_<FLT>& next_frame_flt) {
+void draw_box(int size, Mat_<FLT> quadrado, Mat_<FLT> next_frame_flt) {
     Mat_<FLT> templ = quadrado;
-    resize(templ, templ, Size(size, size), 0, 0, INTER_AREA);
+    resize(templ, templ, Size(size, size), 0, 0, INTER_NEAREST);
     line(next_frame_flt, Point(min_max.match_loc.x + templ.cols / 2, min_max.match_loc.y + templ.rows / 2 + 0.02 * templ.rows), Point(min_max.match_loc.x + templ.cols / 2, min_max.match_loc.y + templ.rows / 2 - 0.02 * templ.rows), Scalar(0, 255, 255), 1, 8);
     line(next_frame_flt,Point(min_max.match_loc.x + templ.cols / 2 + 0.02 * templ.rows, min_max.match_loc.y + templ.rows / 2),Point(min_max.match_loc.x + templ.cols / 2 - 0.02 * templ.rows, min_max.match_loc.y + templ.rows / 2),Scalar(0, 255, 255), 1, 8);
     rectangle(next_frame_flt, min_max.match_loc, Point(min_max.match_loc.x + templ.cols, min_max.match_loc.y + templ.rows), Scalar(0, 255, 255), 1.5, 8);
@@ -71,6 +71,8 @@ int main(int argc, char* argv[]) {
     std::string video_file = argv[1];
     std::string quadrado_file = argv[2];
     std::string output_file = argv[3];
+
+    namedWindow("Janela");
 
     // leitura do quadrado
     Mat_<FLT> quadrado;
@@ -121,7 +123,7 @@ int main(int argc, char* argv[]) {
 
         last_size = next_size;
         Mat_<FLT> quadrado_temp;
-        resize(quadrado, quadrado_temp, Size(next_size, next_size), 0, 0, INTER_AREA);
+        resize(quadrado, quadrado_temp, Size(next_size, next_size), 0, 0, INTER_NEAREST);
         quadrado_temp = somaAbsDois(dcReject(quadrado_temp, 1.0));
 
         matchTemplate(next_frame_flt, quadrado_temp, result, CV_TM_CCORR);
@@ -129,7 +131,7 @@ int main(int argc, char* argv[]) {
         minMaxLoc(result, &min_max.min_val, &min_max.max_val, &min_max.min_loc, &min_max.max_loc);
         if (is_last_frame == true) {
             if (pow((min_max.max_loc.x - min_max.last_loc.x), 2) + pow((min_max.max_loc.y - min_max.last_loc.y), 2) < 400 && min_max.max_val > 0.10) {
-                resize(quadrado, quadrado_temp, Size(next_size, next_size), 0, 0, INTER_AREA);
+                resize(quadrado, quadrado_temp, Size(next_size, next_size), 0, 0, INTER_NEAREST);
                 matchTemplate(next_frame_flt, quadrado_temp, result_normed, CV_TM_CCOEFF_NORMED);
                 minMaxLoc(result_normed, &min_max_normed.min_val, &min_max_normed.max_val, &min_max_normed.min_loc, &min_max_normed.max_loc);
 
@@ -142,7 +144,7 @@ int main(int argc, char* argv[]) {
 
                     is_last_frame = true;
                     min_max.last_loc = min_max.match_loc;
-                    draw_box(next_size, quadrado, next_frame_flt);
+                    draw_box(next_size, quadrado, next_frame_loop);
                 } else {
                     is_last_frame = false;
                 }
@@ -151,7 +153,7 @@ int main(int argc, char* argv[]) {
             } 
         } else {
             if (min_max.max_val > 0.18) {
-                resize(quadrado, quadrado_temp, Size(next_size, next_size), 0, 0, INTER_AREA);
+                resize(quadrado, quadrado_temp, Size(next_size, next_size), 0, 0, INTER_NEAREST);
                 matchTemplate(next_frame_flt, quadrado_temp, result_normed, CV_TM_CCOEFF_NORMED);
                 minMaxLoc(result_normed, &min_max_normed.min_val, &min_max_normed.max_val, &min_max_normed.min_loc, &min_max_normed.max_loc);
 
@@ -164,7 +166,7 @@ int main(int argc, char* argv[]) {
 
                     is_last_frame = true;
                     min_max.last_loc = min_max.match_loc;
-                    draw_box(next_size, quadrado, next_frame_flt);
+                    draw_box(next_size, quadrado, next_frame_loop);
                 } else {
                     is_last_frame = false;
                 }
@@ -172,7 +174,7 @@ int main(int argc, char* argv[]) {
                 is_last_frame = false;
             }
         }
-
+        imshow("Janela", next_frame_loop);
         vo << next_frame_loop;
     }
     TimePoint t2 = timePoint();
